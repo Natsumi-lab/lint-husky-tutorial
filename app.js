a3eb8e01437fbc00230c63e56d361852d6b388f7
Natsumi-lab <imustan81@gmail.com>
2025-11-30 16:15:03 +0900

    pre-commitのテスト

--- app.js
+++ app.js
@@ -10,3 +10,9 @@ const helloworld
 
 const message = 'This is a test message';
     console.log(message);
+
+const button = document.getElementById('btn');
+
+button.addEventListener("click", function() {
+    console.log("クリック");
+});
